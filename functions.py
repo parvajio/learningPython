e2c463ce874a8404bb5be91d6cdd393377a5a4be
parvajio <mdparvajmosharof17@gmail.com>
2025-08-01 23:06:39 +0600

    functions

---
+++ functions.py
@@ -0,0 +1,26 @@
+#functions = a block of reusable code
+            # place () after function name to revoke it
+
+#return = statement used ot end a function and SEND a result to the caller
+
+# def hihelo(name):
+#     print(f"AssalamuAlaikum, boro vai, {name}")
+#     print("Ei boro voy dekhaiyen na")
+#     print("okey")
+#
+# hihelo("shakib")
+# hihelo("rakib")
+# hihelo("yonus")
+
+# def display_invoice(username, amount, due_date):
+#     print(f"hello, {username}")
+#     print(f"Your bill of ${amount:.2f} is due: {due_date}")
+#
+# display_invoice("parvajio", 33.3333, "01/23")
+
+# def add(x,y):
+#     z = x + y
+#     return z
+#
+# r = add(2,44)
+# print(r)
