b8dc6389e362c724a14677eae6aa58101dad759b
parvajio <mdparvajmosharof17@gmail.com>
2025-08-01 22:39:04 +0600

    for loops

---
+++ forLoops.py
@@ -0,0 +1,25 @@
+# for loops = execute a block of code a fixed number of times.
+#               You can iterate over a range, string, sequence, etc.
+
+# for x in range(1, 11):
+#     print(x)
+
+# for x in reversed(range(1, 11)):
+#     print(x)
+#
+# print("Happy")
+
+# for x in range(1,11,4):
+#     print(x)
+
+# my_name = "parvaj"
+# for spell in my_name:
+#     print(spell)
+
+# for x in range(1, 6):
+#     if x == 3:
+#         continue #breack (if want to stop here)
+#     else:
+#         print(x)
+
+
