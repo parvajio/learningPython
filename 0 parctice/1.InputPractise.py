d7537ca4b3ee5fe121a0278e5f77d4f77c542c9b
parvajio <mdparvajmosharof17@gmail.com>
2025-08-01 09:23:45 +0600

    area calculation

--- 0 parctice/1.InputPractise.py
+++ 0 parctice/1.InputPractise.py
@@ -1,13 +1,22 @@
 #mad libs
 
-adjective1 = input("Enter an Adjectvie : ")
-noun = input("Enter a noun : ")
-adjective2 = input("Enter an Adjectvie :")
-verb = input("Enter a verb : ")
-adjective3 = input("Enter an Adjectvie : ")
+# adjective1 = input("Enter an Adjectvie : ")
+# noun = input("Enter a noun : ")
+# adjective2 = input("Enter an Adjectvie :")
+# verb = input("Enter a verb : ")
+# adjective3 = input("Enter an Adjectvie : ")
+#
+#
+# print(f"Today I went to a {adjective1} zoo")
+# print(f"In an exhibit, I saw {noun}")
+# print(f"{noun} was {adjective2} and {verb}ing")
+# print(f"I was {adjective3}")
 
+#area cal
+# l = int(input("Enter the length : "))
+# w = int(input("Enter width : "))
+#
+# area = l*w
+#
+# print(area)
 
-print(f"Today I went to a {adjective1} zoo")
-print(f"In an exhibit, I saw {noun}")
-print(f"{noun} was {adjective2} and {verb}ing")
-print(f"I was {adjective3}")
