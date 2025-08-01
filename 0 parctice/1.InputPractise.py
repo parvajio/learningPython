ea137fcfd1977dd98dd4d4eb61b4c1d72d0134ba
parvajio <mdparvajmosharof17@gmail.com>
2025-08-01 09:18:37 +0600

    mad libs game with input

---
+++ 0 parctice/1.InputPractise.py
@@ -0,0 +1,13 @@
+#mad libs
+
+adjective1 = input("Enter an Adjectvie : ")
+noun = input("Enter a noun : ")
+adjective2 = input("Enter an Adjectvie :")
+verb = input("Enter a verb : ")
+adjective3 = input("Enter an Adjectvie : ")
+
+
+print(f"Today I went to a {adjective1} zoo")
+print(f"In an exhibit, I saw {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"I was {adjective3}")
--- 3.input.py
+++ 3.input.py
@@ -1,2 +1,6 @@
-name = input("Enter your name")
-print(f"You are not {name}")
+# name = input("Enter your name")
+# print(f"You are not {name}")
+
+age = int(input("enter your age"))
+age = age + 1
+print(age)
