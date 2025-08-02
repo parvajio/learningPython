b9fe37e8e91999b5b10ed19ae1d67b71c771f131
parvajio <mdparvajmosharof17@gmail.com>
2025-08-02 21:20:11 +0600

    read

---
+++ read.py
@@ -0,0 +1,21 @@
+# file_path = "C:/Users/parvajio/Desktop/input.txt"
+
+# try:
+#     with open(file_path, "r") as file:
+#         content = file.read()
+#         print(content)
+# except FileNotFoundError:
+#     print("File not found.")
+# except PermissionError:
+#     print("Permission denied.")
+
+# import json
+# json_file = "C:/Users/parvajio/Desktop/input.json"
+
+# try:
+#     with open(json_file, "r") as file:
+#         content = json.load(file)
+#         print(content[0]["content"])
+# except FileNotFoundError:
+#     print("file not found")
+
