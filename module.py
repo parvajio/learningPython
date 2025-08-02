d8e295d36644038ae4fb65149bf431ea396f5260
parvajio <mdparvajmosharof17@gmail.com>
2025-08-02 20:39:34 +0600

    modules

---
+++ module.py
@@ -0,0 +1,26 @@
+# module = a file containing code you want to include in your program
+#           use'import' to include a module (built-in or your own)
+#           useful to break up a large program reusable separate files
+
+# import math
+# import math as m
+# from math import pi
+
+# print(pi)
+# print("high")
+
+# import calendar
+
+# cal = calendar.month(2025, 8)
+# print(cal)
+
+# print(calendar.isleap(2024))  # Check if 2024 is a leap year
+
+# import ownmodule
+
+# print(ownmodule.pi)
+
+# print(ownmodule.sqr(5))
+# print(ownmodule.cude(3))
+
+# print(help("modules"))
---
+++ ownmodule.py
@@ -0,0 +1,7 @@
+pi = 3.1416
+
+def sqr(x):
+    return x * x
+
+def cude(x):
+    return x * x * x
