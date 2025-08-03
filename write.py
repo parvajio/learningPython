73b8b0ad0e978accdd079dd09f435db8a72654f5
parvajio <mdparvajmosharof17@gmail.com>
2025-08-03 10:07:26 +0600

    write txt, json file

---
+++ write.py
@@ -0,0 +1,34 @@
+# txt_data = "HI, I am MD. Parvaj Mosharof"
+
+# file_path = "C:/Users/parvajio/Desktop/output.txt"
+
+# try:
+#     with open(file_path, "a") as file: #"w" for write, "x" for not existing file, "a" for add content repeatedly in the file
+#         file.write("\n"+txt_data)
+#         print(f"txt file '{file_path}' was created.")
+# except FileExistsError:
+#     print("file already exists.")
+
+# fruites = ["apple", "mango", "pineapple"]
+
+# file_path = "output.txt"
+
+# with open(file_path, "w") as file:
+#     for fruit in fruites:
+#         file.write(fruit + "\n")
+#     print("created file")
+
+# import json
+
+# employee = {
+#     "name" : "Md. Parvaj Mosharof",
+#     "age" : 21,
+#     "email" : "parvajio25@gmail.com"
+# }
+
+# json_path = "outputJson.json"
+
+# with open(json_path, "w") as file:
+#     json.dump(employee, file, indent=4)
+#     print("json file creted")
+
